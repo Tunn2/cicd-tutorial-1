b4a6cba3c3c72840bd7dbc99178f71202e3a77b4
tungnk <tungnk1404@gmail.com>
2025-06-25 23:50:06 +0700

    siuuu

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.WebHost.UseUrls("http://*:3000");
+builder.WebHost.UseUrls("http://*:8080");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
